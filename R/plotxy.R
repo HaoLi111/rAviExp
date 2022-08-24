8b25fb44f22f194e384baeda9938495d0f39043a
Hao <32727671+HaoLi111@users.noreply.github.com>
2022-08-23 18:06:06 -0700

    Update plotxy.R

--- R/plotxy.R
+++ R/plotxy.R
@@ -20,25 +20,27 @@ plotxy.wing<-function(wing,add=F,col='black',lty=1){
   }
 }
 
-pointsxy.wing<-function(wing,col='black',lty=1){
-  #if(missing(wing$Dihedral)) wing$Dihedral=0
-  if(wing$Type ==1){
-    span<- wing$Span
-  }else{
-    span<-wing$Span/2
-   }
-  x=wing$x;y=wing$y;z=wing$z
-  front<-c(x,y,z)
-  #dim(front) = c(1,3)
-  rear<-c(x+wing$ChordR,y,z*wing$Dihedral)
-  #dim(rear) = c(1,3)
-  frontT<-c(x+span*sind(wing$Sweep),y+span*cosd(wing$Sweep),z+span*cosd(wing$Dihedral))
-  #dim(frontT) = c(1,3)
-  rearT<-c(x+span*sind(wing$Sweep)+wing$ChordT,y+span*cosd(wing$Sweep),z+span*cosd(wing$Dihedral))
-  #dim(rearT) = c(1,3)
-  pointCloud<-rbind(as.numeric(front)[1:2],as.numeric(frontT)[1:2],as.numeric(rearT)[1:2],as.numeric(rear)[1:2])
-  points(pointCloud[,1],pointCloud[,2],xlab='x',ylab='y',type='l',asp=1,col=col,lty=lty)
-  
+pointsxy.wing =function (wing, col = "black", lty = 1) 
+{
+  if (wing$Type == 1) {
+    span <- wing$Span
+  }
+  else {
+    span <- wing$Span/2
+    x = wing$x
+    y = wing$y
+    z = wing$z
+    front <- c(x, y, z)
+    rear <- c(x + wing$ChordR, y, z * wing$Dihedral)
+    frontT <- c(x + span * tand(wing$Sweep), y + span * cosd(wing$Dihedral), 
+                z + span * cosd(wing$Dihedral))
+    rearT <- c(x + span * tand(wing$Sweep) + wing$ChordT, 
+               y + span * cosd(wing$Dihedral), z + span * cosd(wing$Dihedral))
+    pointCloud <- rbind(as.numeric(front)[1:2], as.numeric(frontT)[1:2], 
+                        as.numeric(rearT)[1:2], as.numeric(rear)[1:2])
+    plot(pointCloud[, 1], pointCloud[, 2], xlab = "x", ylab = "y", 
+         type = "l", asp = 1, col = col, lty = lty)
+  }
 }
 
 plotxy.fuse<-function(fuse,add=F,col='black',lty=1){
